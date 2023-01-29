d1a1faa4b49fc6e0e6512df7654f11cd0ba428fd
Edward Tanguay (windows) <edward@tanguay.info>
2023-01-29 17:13:49 +0100

    import page in

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.scss';
 import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
 import { PageFlashcards } from './pages/PageFlashcards';
 import { PageLearn } from './pages/PageLearn';
+import { PageImport } from './pages/PageImport';
 
 function App() {
 	return (
@@ -9,11 +10,13 @@ function App() {
 			<h1>Learn Core</h1>
 			<nav>
 				<NavLink to="/learn">Learn</NavLink>
+				<NavLink to="/import">Import</NavLink>
 				<NavLink to="/flashcards">Flashcards</NavLink>
 			</nav>
 
 			<Routes>
 				<Route path="/learn" element={<PageLearn />} />
+				<Route path="/import" element={<PageImport />} />
 				<Route path="/flashcards" element={<PageFlashcards />} />
 				<Route path="/" element={<Navigate to="/learn" replace />} />
 			</Routes>
---
+++ src/pages/PageImport.tsx
@@ -0,0 +1,7 @@
+export const PageImport = () => {
+	return (
+		<>
+			<p>This is the Import page.</p>
+		</>
+	);
+};
