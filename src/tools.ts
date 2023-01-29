f392fec7949482d58757abfc9539a3b35aa520be
Edward Tanguay (windows) <edward@tanguay.info>
2023-01-29 20:22:39 +0100

    date nnn

--- src/tools.ts
+++ src/tools.ts
@@ -1,14 +1,19 @@
 import * as tools from './tools';
 
 export const convertStringBlockToLines = (stringBlock: string) => {
-	const lines = stringBlock.split('\n').map(m => m.trim());
-	return lines;
+	if (stringBlock.trim() === '') {
+		return [];
+	} else {
+		const lines = stringBlock.split('\n').map(m => m.trim());
+		return lines;
+	}
 }
 export const buildSpanishFlashcardJsonText = (spanishImportText: string) => {
 	const lines = tools.convertStringBlockToLines(spanishImportText);
+	console.log(lines);
 	const lineBlocks = tools.getLineBlocksFromLines(lines);
-	// const json = tools.getSpanishJsonFromLineBlocks(lineBlocks);
-	console.log(lineBlocks)
+	const json = tools.getSpanishJsonFromLineBlocks(lineBlocks);
+	return json;
 }
 
 export const getLineBlocksFromLines = (lines: string[]) => {
@@ -28,7 +33,6 @@ export const getLineBlocksFromLines = (lines: string[]) => {
 	// pad so all have same length entries
 	for (const lineBlock of lineBlocks) {
 		const numberOfLinesToAdd = 4 - lineBlock.length;
-		console.log(lineBlock.length, numberOfLinesToAdd);
 		for (let i = 0; i < numberOfLinesToAdd; i++) {
 			lineBlock.push('');
 		}
@@ -36,13 +40,24 @@ export const getLineBlocksFromLines = (lines: string[]) => {
 	return lineBlocks;
 }
 
-// export const getSpanishJsonFromLineBlocks = (lineBlocks: string[][]) => {
-// 	const objs = [];
-// 	for (const lineBlock of lineBlocks) {
-// 		const obj = {
-// 			category: 'spanish',
-// 			subcategory ''
-// 		}
-// 	}
-// }
+export const getSpanishJsonFromLineBlocks = (lineBlocks: string[][]) => {
+	const objs = [];
+	for (const lineBlock of lineBlocks) {
+		const obj = {
+			category: 'spanish',
+			subcategory: lineBlock[3],
+			front: lineBlock[0],
+			back: lineBlock[1],
+			rank: lineBlock[2] === '' ? 2.5 : Number(lineBlock[2]),
+			whenCreated: tools.getDateTime()
+		}
+		objs.push(obj);
+	}
+	console.log(objs);
+	return JSON.stringify(objs);
+}
+
+export const getDateTime = () => {
+	return 'nnn';
+}
 
