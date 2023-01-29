25291a8c26b36a048510ece9b860950b242c79bb
Edward Tanguay (windows) <edward@tanguay.info>
2023-01-29 18:58:36 +0100

    import form styled

--- src/pages/PageImport.tsx
+++ src/pages/PageImport.tsx
@@ -1,7 +1,33 @@
 export const PageImport = () => {
+
+	const formatExampleText = `
+I'm sorry, I do not understand you. I still can't understand you.
+Lo siento, no te entiendo. Todavía no puedo entenderte.
+cantUnderstand
+
+What time is it?
+¿Qué hora será?
+
+My Spanish isn't good enough to understand.
+Mi español no es suficiente para entenderte.
+cantUnderstand
+
+I will do it.
+Lo haré.
+`;
 	return (
 		<div className="page pageImport">
-			<p>This is the Import page.</p>
+			<form>
+				<div className="row">
+					<label>Spanish Flashcards</label>
+					<textarea></textarea>
+				</div>
+				<div className="buttonArea">
+					<button type="button">Import</button>
+				</div>
+			</form>
+			<h2>Format example</h2>
+			<pre>{formatExampleText.trim()}</pre>
 		</div>
 	);
 };
