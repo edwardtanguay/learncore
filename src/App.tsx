91c9bdf589fccfec4c7fe41166b3edb66fdd6b28
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-18 00:21:29 +0100

    pageverbs started

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
 import { PageFlashcards } from './pages/PageFlashcards';
 import { PageLearn } from './pages/PageLearn';
 import { PageImport } from './pages/PageImport';
+import { PageBaseVerbs } from './pages/PageBaseVerbs';
 
 function App() {
 	return (
@@ -12,12 +13,14 @@ function App() {
 				<NavLink to="/learn">Learn</NavLink>
 				<NavLink to="/import">Import</NavLink>
 				<NavLink to="/flashcards">Flashcards</NavLink>
+				<NavLink to="/baseverbs">Base Verbs</NavLink>
 			</nav>
 
 			<Routes>
 				<Route path="/learn" element={<PageLearn />} />
 				<Route path="/import" element={<PageImport />} />
 				<Route path="/flashcards" element={<PageFlashcards />} />
+				<Route path="/baseverbs" element={<PageBaseVerbs />} />
 				<Route path="/" element={<Navigate to="/learn" replace />} />
 			</Routes>
 		</div>
