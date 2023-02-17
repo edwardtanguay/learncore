eff62f51f23cb4a2a30d653d177f3a3f22bc9cde
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-18 00:42:04 +0100

    verb endings in

--- src/pages/PageBaseVerbs.tsx
+++ src/pages/PageBaseVerbs.tsx
@@ -6,34 +6,47 @@ export const PageBaseVerbs = () => {
 			"pres": ['o', 'as', 'a', 'amos', 'áis', 'an'],
 			"impe": ['aba', 'abas', 'aba', 'ábabos', 'abais', 'aban'],
 			"pret": ['é', 'aste', 'ó', 'amos', 'asteis', 'arán'],
-			"futu": ['ará', 'aras'],
-			"cond": ['','','','','',''],
-			"subj": ['','','','','',''],
+			"futu": ['é', 'ás','á','emos','éis','án'],
+			"cond": ['ía','ías','ía','íamos','íais','ían'],
+			"subj": ['e','es','e','emos','éis','en'],
 		},
 		"er": {
-			"part": ['','','','','',''],
-			"pres": ['','','','','',''],
-			"impe": ['','','','','',''],
-			"pret": ['','','','','',''],
-			"futu": ['','','','','',''],
-			"cond": ['','','','','',''],
-			"subj": ['','','','','',''],
+			"part": ['iendo','ido'],
+			"pres": ['o','es','e','emos','éis','en'],
+			"impe": ['ía','ías','ía','íamos','íais','ían'],
+			"pret": ['í','iste','ió','imos','isteis','ieron'],
+			"futu": ['é', 'ás','á','emos','éis','án'],
+			"cond": ['ía','ías','ía','íamos','íais','ían'],
+			"subj": ['a','as','a','amos','áis','an'],
 		},
 		"ir": {
-			"part": ['','','','','',''],
-			"pres": ['','','','','',''],
-			"impe": ['','','','','',''],
-			"pret": ['','','','','',''],
-			"futu": ['','','','','',''],
-			"cond": ['','','','','',''],
-			"subj": ['','','','','',''],
+			"part": ['iendo','ido'],
+			"pres": ['o','es','e','imos','ís','en'],
+			"impe": ['ía','ías','ía','íamos','íais','ían'],
+			"pret": ['í','iste','ió','imos','isteis','ieron'],
+			"futu": ['é', 'ás','á','emos','éis','án'],
+			"cond": ['ía','ías','ía','íamos','íais','ían'],
+			"subj": ['a','as','a','amos','áis','an'],
 		}
 	}
+
+	const verbs = ['hablar, comer, vivir'];
 
 
 	return (
 		<>
-			<p>This is the BaseVerbs page.</p>
+			<table>
+				<tbody>
+					<tr>
+						<td>nnn</td>
+						<td>nnn</td>
+						<td>nnn</td>
+						<td>nnn</td>
+						<td>nnn</td>
+						<td>nnn</td>
+					</tr>
+				</tbody>
+		</table>
 		</>
 	);
 };
