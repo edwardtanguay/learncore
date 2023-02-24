48f21f291e2ab216ea1eaad7c881361f42618dfc
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-24 23:00:26 +0100

    change many

---
+++ src/data/verbDefinitions.ts
@@ -0,0 +1,81 @@
+const verbDefinitions = `
+
+ser: 1soy, 2eres, 3es, 4somos, 5sois, 6son; 1era, 2eras, 3era, 4éramos, 5erais, 6eran; 1fui, 2fuiste, 3fue, 4fuimos, 5fuisteis, 6fueron // -; -; - // -; -
+estar: 1estoy, 2estás, 3está, 6están; -; 1estuve, 2estuviste, 3estuvo, 4estuvimos; 5estuvisteis, 6estuvieron // -; -; 1esté, 2estés, 3esté, 6estén // -; -
+ir: 1voy, 2vas, 3va, 4vamos, 5vais, 6va; 1iba, 2ibas, 3iba, 4íbamos, 5ibais, 6iban; 1fui, 2fuiste, 3fue, 4fuimos, 5fuisteis, 6fueron // -; -; vay- // yendo; -
+haber: 1he, 2has, 3ha, 4hemos, 6han; -; hub-, 1hube, 3hubo // hubr-; hubr-; hay // -; -
+
+poder: pued-,4-,5-;-;pud-,1pude,3pudo//podr-;podr-;peud-,4-,5- // pudiendo; -
+poner: 1pongo; -; pus-, 1puse, 3puso // pondr-; pondr-; pong- // -; poner
+querer: quier-, 4-, 5-; -; quis-, 1quise, 2quiso // querr-; querr-; quier-, 4-, 5- // -; - 
+deber
+desear
+hacer: 1hago ; hac- ; hic-,1hice,3hizo // har- ; har- ; hag- // - ; hecho
+tener: tien-,1tengo,4-,5- ; - ; tuv- // tendr- ; tendr- ; teng- // - ; -
+dar: 1doy, 5dais; -; 1di, 2diste, 3dio, 4dimos, 5disteis, 6dieron // -; -; 1dé, 2des, 3dé, 4demos, 5deis, 6den // -; -
+saber: 1sé; -; sup-, 1supe, 3supo // sabr-; sabr-; sep- // -; -
+ver: 1veo, 5veis; ve-; 1vi, 3vio // -; -; ve- // -; visto
+
+venir: vien-, 1vengo, 4-, 5-; -; vin-, 1vine, 3vino // vendr-; vendr-; veng- // viniendo; -
+vender
+
+llegar: - ; - ; 1llegué // - ; - ; llegu- // - ; -
+llamar
+
+hablar
+instalar
+ejecutar
+conectar
+separar
+crear
+gritar
+notar
+evitar
+levantar
+segmentar
+revisar
+implementar
+guardar
+generar
+cambiar
+almacenar
+
+comer
+correr
+suceder
+
+vivir
+unir
+permitir
+recibir
+definir
+
+// AR IRREGULAR
+arrancar: -; -; 1arranqué // -; -; arranqu- // -; -
+desplegar: desplieg-, 4-, 5-; -; 1desplegué // -; -; despliegu- // -; -
+mostrar: muestr-, 4-, 5-; -; - // -; -; muestr- // -; -
+
+// AR: o-to-ue
+recordar: recuerd-,4-,5- ; - ; - // - ; - ; recuerd-,4-,5- // - ; -
+encontrar: encuentr-, 4-, 5-; -; - // -; -; encuentr-, 4-, 5- // -; -
+
+// AR: g-to-gu
+agregar: -; -; agregué // -; -; agregu- // -; -
+pegar: -; -; pegué // -; -; pegu // -; -
+
+// AR: z-to-c
+utilizar: -; -; 1utilicé // -; -; utilic- // -; -
+realizar: -; -; 1realicé // -; -; realic- // -; -
+
+// ER IRREGULAR
+ofrecer: 1ofrezco;-;-//-;-;ofrezc-//-;-
+aparecer: 1aparezco; -; - // -; -; aparezc- // -; -
+volver: vuelv-,4-,5- ; - ; - // - ; - ; vuelv-,4-,5- // - ; vuelto
+
+// IR IRREGULAR
+incluir: incluy-,4-,5- ; - ; 3incluy-, 6incluy- // - ; - ; incluy- // incluyendo ; -	
+convertir: conviert-, 4-, 5-; -; 3convirtió, 6convirtieron // -; -; conviert-, 4convirt-, 5convirt- // convirtiendo; -
+abrir: -; -; - // -; -; - // -; abierto
+`;
+
+export default verbDefinitions;
--- src/parser.ts
+++ src/parser.ts
@@ -1,63 +1,8 @@
 import { IConjugatedVerb, IConjugationEndings, ISummaryVerbGroups, IVerb } from "./interfaces";
 import * as qstr from './qtools/qstr';
+import verbDefinitions from "./data/verbDefinitions";
 
 // TODO: add rank at end and buttons: Most Recent, Most Important, Base Conjugations
-
-export const verbDefinitions = `
-
-ser: 1soy, 2eres, 3es, 4somos, 5sois, 6son; 1era, 2eras, 3era, 4éramos, 5erais, 6eran; 1fui, 2fuiste, 3fue, 4fuimos, 5fuisteis, 6fueron // -; -; - // -; -
-estar: 1estoy, 2estás, 3está, 6están; -; 1estuve, 2estuviste, 3estuvo, 4estuvimos; 5estuvisteis, 6estuvieron // -; -; 1esté, 2estés, 3esté, 6estén // -; -
-ir: 1voy, 2vas, 3va, 4vamos, 5vais, 6va; 1iba, 2ibas, 3iba, 4íbamos, 5ibais, 6iban; 1fui, 2fuiste, 3fue, 4fuimos, 5fuisteis, 6fueron // -; -; vay- // yendo; -
-haber: 1he, 2has, 3ha, 4hemos, 6han; -; hub-, 1hube, 3hubo // hubr-; hubr-; hay // -; -
-
-poder: pued-,4-,5-;-;pud-,1pude,3pudo//podr-;podr-;peud-,4-,5- // pudiendo; -
-querer: quier-, 4-, 5-; -; quis-, 1quise, 2quiso // querr-; querr-; quier-, 4-, 5- // -; - 
-deber
-desear
-hacer: 1hago ; hac- ; hic-,1hice,3hizo // har- ; har- ; hag- // - ; hecho
-tener: tien-,1tengo,4-,5- ; - ; tuv- // tendr- ; tendr- ; teng- // - ; -
-dar: 1doy, 5dais; -; 1di, 2diste, 3dio, 4dimos, 5disteis, 6dieron // -; -; 1dé, 2des, 3dé, 4demos, 5deis, 6den // -; -
-saber: 1sé; -; sup-, 1supe, 3supo // sabr-; sabr-; sep- // -; -
-ver: 1veo, 5veis; ve-; 1vi, 3vio // -; -; ve- // -; visto
-
-venir: vien-, 1vengo, 4-, 5-; -; vin-, 1vine, 3vino // vendr-; vendr-; veng- // viniendo; -
-vender
-
-llegar: - ; - ; 1llegué // - ; - ; llegu- // - ; -
-llamar
-
-hablar
-instalar
-ejecutar
-conectar
-separar
-crear
-gritar
-notar
-evitar
-levantar
-segmentar
-revisar
-
-comer
-correr
-
-vivir
-unir
-permitir
-recibir
-
-arrancar: -; -; 1arranqué // -; -; arranqu- // -; -
-recordar: recuerd-,4-,5- ; - ; - // - ; - ; recuerd-,4-,5- // - ; - 
-utilizar: -; -; 1utilicé // -; -; utilic- // -; -
-
-ofrecer: 1ofrezco;-;-//-;-;ofrezc-//-;-
-aparecer: 1aparezco; -; - // -; -; aparezc- // -; -
-volver: vuelv-,4-,5- ; - ; - // - ; - ; vuelv-,4-,5- // - ; vuelto
-
-incluir: incluy-,4-,5- ; - ; 3incluy-, 6incluy- // - ; - ; incluy- // incluyendo ; -	
-convertir: conviert-, 4-, 5-; -; 3convirtió, 6convirtieron // -; -; conviert-, 4convirt-, 5convirt- // convirtiendo; -
-`;
 
 const getRegularVerbs = (verbDefinitions: string): IVerb[] => {
 	const lines = qstr.convertStringBlockToLines(verbDefinitions);
